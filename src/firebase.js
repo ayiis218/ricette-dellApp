ae5ac100bcf8058cfd1bb6565d1a4898b86ce9dc
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-07-18 10:08:05 +0700

    add

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { GoogleAuthProvider } from 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import "firebase/compat/auth"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBRy04JYAb4kEYRv2dBPjsRCgrAVDWvVnA",
+    authDomain: "ricette-test.firebaseapp.com",
+    projectId: "ricette-test",
+    storageBucket: "ricette-test.appspot.com",
+    messagingSenderId: "907645431962",
+    appId: "1:907645431962:web:c551714a4f9d9e460aef23",
+    measurementId: "G-BNR6K100EV"
+  };
+
+const app = initializeApp(firebaseConfig)
+
+const analytics = getAnalytics(app)
+const provider = new GoogleAuthProvider()
+
+export { provider, analytics, app }
