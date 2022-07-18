d6506607f906e5a375789bc0c7f8f22bde6d2cb2
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-07-18 07:32:49 +0700

    deploy

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import "firebase/compat/auth"
 
 
 const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
-}
+    apiKey: "AIzaSyAi0HOP9C9pkz_6vrfwcrhiCTZnaGRJ2EE",
+    authDomain: "ricette-dellapp.firebaseapp.com",
+    projectId: "ricette-dellapp",
+    storageBucket: "ricette-dellapp.appspot.com",
+    messagingSenderId: "457417238580",
+    appId: "1:457417238580:web:20e077684711881abc8261",
+    measurementId: "G-3LF1QNBS2X"
+  };
 
 const app = initializeApp(firebaseConfig)
 
